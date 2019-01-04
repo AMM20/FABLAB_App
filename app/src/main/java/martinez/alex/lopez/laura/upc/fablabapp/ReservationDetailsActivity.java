165617e4db211e2fffe4d0f56243078fc557f455
Alex <alexmmorange97@gmail.com>
2019-01-04 17:17:25 +0100

    Laura + Alex: V0.6.1: Disseny del layout de l'activitat ReservationDetailsActivity

---
+++ app/src/main/java/martinez/alex/lopez/laura/upc/fablabapp/ReservationDetailsActivity.java
@@ -0,0 +1,13 @@
+package martinez.alex.lopez.laura.upc.fablabapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_details);
+    }
+}
