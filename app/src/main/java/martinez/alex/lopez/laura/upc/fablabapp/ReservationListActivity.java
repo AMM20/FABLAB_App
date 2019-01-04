e8b74a19971689123157ba8bc7f6a2517c9818fc
Alex <alexmmorange97@gmail.com>
2019-01-04 17:09:28 +0100

    Creació de l'aplicació FABLAB App.

---
+++ app/src/main/java/martinez/alex/lopez/laura/upc/fablabapp/ReservationListActivity.java
@@ -0,0 +1,13 @@
+package martinez.alex.lopez.laura.upc.fablabapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_list);
+    }
+}
